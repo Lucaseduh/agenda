f4148741b30f703ea4cd5b5a43286888f8a2ddee
Lucas Eduardo <103043994+Lucaseduh@users.noreply.github.com>
2023-05-24 10:23:31 -0300

    Add files via upload

---
+++ Agenda.py
@@ -0,0 +1,121 @@
+import os
+os.system ('cls')
+
+agenda = []
+
+def pede_nome():
+    return input("Nome: ")
+def pede_telefone():
+    return input("Telefone: ")
+def mostra_dados(nome,telefone):
+    print(f"Nome: {nome} Telefone: {telefone}")
+def pede_nome_arquivo():
+    return input("Nome do arquivo: ")
+def pesquisa(nome):
+    mnome = nome.lower()
+    for p, e in enumerate(agenda):
+        if e[0].lower()==mnome:
+            return p
+    return None
+def novo():
+    global agenda
+    nome = pede_nome()
+    telefone = pede_telefone()
+    agenda.append([nome,telefone])
+def apaga():
+    global agenda
+    nome = pede_nome()
+    p = pesquisa(nome)
+    if p is not None:
+        del agenda[p]
+    else:
+        print("Nome não encontrado")
+def altera():
+    p = pesquisa(pede_nome())
+    if p is not None:
+        nome = agenda[p][0]
+        telefone = agenda[p][1]
+        print("Encontrado")
+        mostra_dados(nome,telefone)
+        nome = pede_nome()
+        telefone = pede_telefone()
+        agenda[p] = [nome,telefone]
+    else:
+        print("Nome não encontrado")
+def lista():
+    print("\nAgenda\n\n------")
+    for e in agenda:
+        mostra_dados(e[0],e[1])
+        print("------\n")
+def le():
+    global agenda
+    nome_arquivo = pede_nome_arquivo()
+    with open (nome_arquivo, "r", encoding="utf8") as arquivo:
+        agenda=[]
+        for l in arquivo.readlines():
+            nome, telefone = l.strip().split("#")
+            agenda.append([nome, telefone])
+def grava():
+    nome_arquivo = pede_nome_arquivo()
+    with open(nome_arquivo, "w", encoding="utf-8") as arquivo:
+        for e in agenda:
+            arquivo.write(f"{e[0]}#{e[1]}\n")
+def valida_faixa_inteiro(pergunta, inicio, fim):
+    while True:
+        try:
+            valor = int(input(pergunta))
+            if inicio <= valor <=fim:
+                return valor
+        except ValueError:
+            print(f"Valor invalido, favor digitar entre {inicio} e {fim}")
+
+#Essa função lê a quantidade de contatos:
+def qto_contatos():
+    print(f"O total de registros é: {[len(agenda)]}")
+
+#Essa função lê os contatos e diz a posição que ele está:
+def posicao_contato():
+    print(f"Tamanho da agenda: {len(agenda)}")
+    for i, contato in enumerate(agenda):
+        nome = contato[0]
+        telefone = contato[1]
+        print(f"Posição: {i} || Nome: {nome} ||Telefone: {telefone}")
+
+        
+
+def menu():
+    print("""
+    1 - Novo
+    2 - Altera
+    3 - Apaga
+    4 - Lista
+    5 - Grava
+    6 - Lê
+    7 - Contagem
+    8 - Exibir a posição do contato
+
+    0 - Sai
+        """)
+    return valida_faixa_inteiro("Escolha uma opção:",0,8)
+
+while True:
+    opcao = menu()
+    if opcao == 0:
+        break
+    elif opcao == 1:
+        novo()
+    elif opcao == 2:
+        altera()
+    elif opcao == 3:
+        apaga()
+    elif opcao == 4:
+        lista()
+    elif opcao == 5:
+        grava()
+    elif opcao == 6:
+        le()
+    elif opcao == 7:
+        qto_contatos()
+    elif opcao == 8:
+        posicao_contato()
+menu()
